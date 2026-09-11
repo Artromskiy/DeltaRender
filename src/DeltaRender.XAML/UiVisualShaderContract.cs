2b6046a8d096d7e4732e7819b9e6a105a1c3216e
Artyom Romskiy <artromskiy@gmail.com>
2026-09-11 22:27:42 +0300

    fix: keep renderer compatible with current XAML contract

--- src/DeltaRender.XAML/UiVisualShaderContract.cs
+++ src/DeltaRender.XAML/UiVisualShaderContract.cs
@@ -702,13 +702,10 @@ internal static class UiVisualShaderContract
             PaintUnits.Device => 1f,
             _ => throw new ArgumentOutOfRangeException(nameof(units), units, "Unknown paint unit system."),
         };
-        var width = layer.SideWidths == default
-            ? layer.Width
-            : Maths.Max(layer.SideWidths.x, Maths.Max(layer.SideWidths.y, Maths.Max(layer.SideWidths.z, layer.SideWidths.w)));
         return new(
             layer.Color,
             layer.Offset * scale,
-            width * scale,
+            layer.Width * scale,
             layer.BlurRadius * scale,
             layer.Spread * scale,
             layer.Intensity);
